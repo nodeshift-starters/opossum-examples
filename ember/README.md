001f12217eda5e11f0d602bcc937ba9ebef420a0
Lucas Holmquist <lholmqui@redhat.com>
2019-09-30 15:39:07 -0400

    chore: update the readme and web app

--- ember/README.md
+++ ember/README.md
@@ -1,6 +1,6 @@
 # Ember Example
 
-This example exposes a simple service at the route `http://localhost:3001/flakeyService`. As the service receives requests, it gets slower and slower. Once it takes more than 1 second to respond, the service just returns a `423 (Locked)` error. Also at the route `http://localhost:3000` has a server running a react app responsible to consume the flakey service.
+This example exposes a simple service at the route `http://localhost:3001/flakeyService`. As the service receives requests, it gets slower and slower. Once it takes more than 1 second to respond, the service just returns a `423 (Locked)` error. Also at the route `http://localhost:4200` has a server running an Ember.js app responsible for consuming the flakey service.
 
 Install dependencies.
 
@@ -17,4 +17,4 @@ Start the server.
 $ npm start
 ```
 
-Browse to `http://localhost:3000` and click the button to see the service in action.
+Browse to `http://localhost:4200` and click the button to see the service in action.
--- ember/client/app/controllers/application.js
+++ ember/client/app/controllers/application.js
@@ -8,6 +8,11 @@ export default Controller.extend({
     this._super(...arguments);
     const self = this;
     this.responses = [];
+    // Adding this just for access in the opossum events
+    // We are specifying the name of the service in the fetch below to get around
+    // CORS.  We've added a --proxy flag in the package.json, so Ember knows
+    // where to the flakeyService is
+    this.route = 'http://localhost:3001/flakeyService';
     this.circuit = new circuitBreaker(() => fetch('flakeyService').then((result) => {
       if (result.status > 399) {
         return Promise.reject(result);
@@ -16,36 +21,36 @@ export default Controller.extend({
       return result;
     }), circuitrBreakerOptions);
     this.circuit.on('success', async (result) => {
-      const res = await result.json();
-      self.responses.pushObject({body: res.body, state: 'SUCCESS'});
+      self.responses.pushObject({body: `${JSON.stringify(await result.json())}`, state: 'SUCCESS'});
     });
     this.circuit.on('timeout', async () => {
-      self.responses.pushObject({ state: `TIMEOUT`, body: `Route is taking too long to respond.` });
+      self.responses.pushObject({ state: `TIMEOUT`, body: `${self.route} is taking too long to respond.` });
     });
     this.circuit.on('open', async () => {
-      self.responses.pushObject({ state: `OPEN`, body: `The breaker for Route just opened.` });
+      self.responses.pushObject({ state: `OPEN`, body: `The breaker for ${self.route} just opened.` });
     });
     this.circuit.on('reject', async () => {
-      self.responses.pushObject({ state: `REJECTED`, body: `The breaker for Route is open. Failing fast.` });
+      self.responses.pushObject({ state: `REJECTED`, body: `The breaker for ${self.route} is open. Failing fast.` });
     });
     this.circuit.on('halfOpen', async () => {
-      self.responses.pushObject({ state: `HALF_OPEN`, body: `The breaker for Route is half open.` });
+      self.responses.pushObject({ state: `HALF_OPEN`, body: `The breaker for ${self.route} is half open.` });
     });
     this.circuit.on('close', async () => {
-      self.responses.pushObject({ state: `CLOSE`, body: `The breaker for Route has closed. Service OK.` });
+      self.responses.pushObject({ state: `CLOSE`, body: `The breaker for ${self.route} has closed. Service OK.` });
     });
     this.circuit.on('fallback', async (result) => {
-      self.responses.pushObject({ state: `FALLBACK`, body: result.body });
+      self.responses.pushObject({ state: `FALLBACK`, body: `${JSON.stringify(result)}` });
     });
     this.circuit.fallback(() => {
-      return {body: 'Route is unavailable right now. Try Later'};
+      return {body: `${self.route} unavailable right now. Try later.`};
     });
   },
   actions: {
     makeRequest: async function () {
-      await this.circuit.fire();//await fetch('/flakeyService');
-      // const json = await response.json();
-      // this.responses.pushObject(json)
+      await this.circuit.fire();
+    },
+    clear: function () {
+      this.set('responses', []);
     }
   }
 });
