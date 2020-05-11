594551575730295114a8d825878b290186e68e91
alexalikiotis <alexalikiotis5@gmail.com>
2020-05-11 14:54:44 +0300

    chore: new react example

--- react/client/src/index.js
+++ react/client/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
