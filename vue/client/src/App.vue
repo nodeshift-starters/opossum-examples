54adfb2d010cec1957a8f42ab58b66a0905d790e
alexalikiotis <alexalikiotis5@gmail.com>
2020-05-12 12:22:56 +0300

    chore: refactoring

--- vue/client/src/App.vue
+++ vue/client/src/App.vue
@@ -17,8 +17,20 @@
       This should allow you to see all of the various events that occur when
       using a circuit breaker.
     </p>
-    <button-list @onRequest="makeRequest" @onClear="clear" />
-    <response-list :list="responseList" />
+    <div>
+      <button @click="makeRequest">Flakey Service</button>
+      <button @click="clear">Clear</button>
+    </div>
+    <div>
+      <p
+        v-for="(element, index) in responseList.slice().reverse()"
+        :key="index"
+        :class="element.state.toLowerCase()"
+      >
+        <span>{{ element.state }} </span>
+        <span>{{ element.body }}</span>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -26,15 +38,8 @@
 import axios from 'axios';
 import opossum from 'opossum';
 
-import ButtonList from './components/ButtonList';
-import ResponseList from './components/ResponseList';
-
 export default {
   name: 'App',
-  components: {
-    ButtonList,
-    ResponseList
-  },
   data() {
     return {
       responseList: [],
@@ -133,5 +138,51 @@ export default {
   color: #2c3e50;
   margin: 0 auto;
   width: 900px;
+}
+
+button {
+  box-shadow: inset 0px 1px 3px 0px #91b8b3;
+  background: linear-gradient(to bottom, #768d87 5%, #6c7c7c 100%);
+  background-color: #768d87;
+  border-radius: 5px;
+  border: 1px solid #566963;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 15px;
+  font-weight: bold;
+  padding: 11px 23px;
+  text-decoration: none;
+  text-shadow: 0px -1px 0px #2b665e;
+  outline: none;
+  margin: 30px 10px 30px 0;
+}
+button:active {
+  background: linear-gradient(to bottom, #6c7c7c 5%, #768d87 100%);
+  background-color: #6c7c7c;
+}
+
+.clear {
+  cursor: pointer;
+  color: #3336ff;
+}
+.success {
+  color: darkgreen;
+}
+.open {
+  color: red;
+}
+.fallback {
+  color: darkblue;
+}
+.rejected {
+  color: darkorange;
+}
+.close {
+  color: green;
+}
+.timeout {
+  color: darksalmon;
 }
 </style>
--- vue/server.js
+++ vue/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const boom = require("@hapi/boom");
 
 const app = express();
