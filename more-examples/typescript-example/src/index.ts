deb041ba19a160267ed168243e93bc84afe4d336
Helio Frota <00hf11@gmail.com>
2021-02-23 11:02:24 -0300

    adding the example with typescript

---
+++ more-examples/typescript-example/src/index.ts
@@ -0,0 +1,22 @@
+import CircuitBreaker from 'opossum';
+
+const delay = (delay) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, delay);
+  });
+
+const breaker = new CircuitBreaker(delay);
+
+breaker
+  .fire(100)
+  .then((_) => {
+    console.log(
+      'fires',
+      breaker.stats.fires,
+      'successes',
+      breaker.stats.successes
+    );
+  })
+  .catch((error) => console.error(error));
